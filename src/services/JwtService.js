fcd131e093aa1fefece0e1866dd169402aa3ed75
Bùi Quang Huy <59251361+huybui76@users.noreply.github.com>
2023-10-13 21:36:13 +0700

    Complete user and product API

--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -1,49 +1,42 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const generalAccessToken = async (payload) => {
-    const access_Token = jwt.sign({ payload }, process.env.ACCESS_TOKEN, { expiresIn: '30s' });
-    return access_Token;
+const generalAccessToken = (payload) => {
+    const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN, { expiresIn: '30s' });
+    return access_token;
 };
 
-const generalRefreshToken = async (payload) => {
-    const refresh_Token = jwt.sign({ payload }, process.env.REFRESH_TOKEN, { expiresIn: '365d' });
-    return refresh_Token;
+const generalRefreshToken = (payload) => {
+    const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN, { expiresIn: '365d' });
+    return refresh_token;
 };
 
-const refreshTokenJwt = (token) => {
+const refreshTokenJwtService = (token) => {
     return new Promise((resolve, reject) => {
-        try {
-            jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
-                if (err) {
-                    return resolve({
-                        status: 'ERROR',
-                        message: 'Token authentication failed',
-                    });
-                }
-
-                const { payload } = user;
-                const access_token = await generalAccessToken({
-                    id: payload?.id,
-                    isAdmin: payload?.isAdmin,
+        jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+            if (err) {
+                resolve({
+                    status: 'ERR',
+                    message: 'Authentication failed'
                 });
-
+            } else {
+                const access_token = generalAccessToken({
+                    id: user?.id,
+                    isAdmin: user?.isAdmin
+                });
                 resolve({
                     status: 'OK',
-                    message: 'REFRESH TOKEN SUCCESS',
-                    access_token,
+                    message: 'SUCCESS',
+                    access_token
                 });
-            });
-        } catch (e) {
-            reject(e);
-        }
+            }
+        });
     });
 };
 
 module.exports = {
     generalAccessToken,
     generalRefreshToken,
-    refreshTokenJwt,
+    refreshTokenJwtService
 };
