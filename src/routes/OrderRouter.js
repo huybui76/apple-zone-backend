70b103ec19ea1994ccb84f0fd3c3af075f274b0f
Bùi Quang Huy <59251361+huybui76@users.noreply.github.com>
2023-11-23 23:11:59 +0700

    fix bug authen

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router()
 const OrderController = require('../controllers/OrderController');
-const { authUserMiddleWare, authMiddleWare } = require("../middleware/authMiddleware");
+const { authMiddleware, authUserMiddleware } = require('../middleware/authMiddleware');
 
-router.post('/create/:id', authUserMiddleWare, OrderController.createOrder)
-router.get('/get-all-order/:id',authUserMiddleWare, OrderController.getAllOrderDetails)
+router.post('/create/:id', authUserMiddleware, OrderController.createOrder)
+router.get('/get-all-order/:id', authUserMiddleware, OrderController.getAllOrderDetails)
 router.get('/get-details-order/:id', OrderController.getDetailsOrder)
-router.delete('/cancel-order/:id',authUserMiddleWare, OrderController.cancelOrderDetails)
-router.get('/get-all-order',authMiddleWare, OrderController.getAllOrder)
+router.delete('/cancel-order/:id', authUserMiddleware, OrderController.cancelOrderDetails)
+router.get('/get-all-order', authMiddleware, OrderController.getAllOrder)
 
 
 module.exports = router
